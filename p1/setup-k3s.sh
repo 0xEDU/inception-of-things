92a4dc7cbe7072e3be1778257f73f91896006ba3
Guiribei <guilherme_f_ribeiro@hotmail.com>
2024-04-04 23:31:06 -0300

    fix: change network on p1

--- p1/setup-k3s.sh
+++ p1/setup-k3s.sh
@@ -1,12 +1,17 @@
 #!/bin/ash
 
 if [[ $(hostname) == "guribeirS" ]]; then
-	curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-ip=192.168.56.110 --flannel-iface=eth1" K3S_TOKEN=4242 sh -
-	k3s="k3s"
-else
-	curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent --server https://192.168.56.110:6443 --token 4242 --flannel-iface=eth1 --node-ip=192.168.56.111" sh -s -
-	k3s="k3s-agent"
+    sudo ip addr replace 192.168.56.110/24 brd 192.168.56.255 dev eth1
+    ip link set eth1 up
+    curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-ip=192.168.56.110 --flannel-iface=eth1" K3S_TOKEN=4242 sh -
+    k3s="k3s"
+    ip route replace 192.168.56.0/24 dev eth1
+elif [[ $(hostname) == "guribeirSW" ]]; then
+    sudo ip addr replace 192.168.56.111/24 brd 192.168.56.255 dev eth1
+    ip link set eth1 up
+    curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent --server https://192.168.56.110:6443 --token 4242 --flannel-iface=eth1 --node-ip=192.168.56.111" sh -s -
+    k3s="k3s-agent"
+    ip route replace 192.168.56.0/24 dev eth1
 fi
-ip route del default
-ip route add default via 192.168.56.1 dev eth1
+
 service $k3s restart
