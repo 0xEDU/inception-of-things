75cfd16e68d4121b9939f6f56a1ffe9d356f5d50
Guiribei <guilherme_f_ribeiro@hotmail.com>
2024-05-03 23:16:16 -0300

    add all

--- bonus/setup-tool.sh
+++ bonus/setup-tool.sh
@@ -1,23 +1,50 @@
 #!/bin/bash
 
-#wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash # install k3d
-#sudo wget -q https://github.com/argoproj/argo-cd/releases/download/v2.10.6/argocd-linux-amd64 -O /usr/local/bin/argocd # install argocd cli
-#sudo chmod 655 /usr/local/bin/argocd # give proper permission to argocd
+# wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash # install k3d
+# sudo wget -q https://github.com/argoproj/argo-cd/releases/download/v2.10.6/argocd-linux-amd64 -O /usr/local/bin/argocd # install argocd cli
+# sudo chmod 655 /usr/local/bin/argocd # give proper permission to argocd
 
-#curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+# curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 
-#sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+# sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
-k3d cluster create p3-cluster -p "8081:8081@loadbalancer" -p "8080:80@loadbalancer" # create cluster
+k3d cluster create bonus-cluster -p "8081:8081@loadbalancer" -p "8080:80@loadbalancer" -p "8082:443@loadbalancer" # create cluster
 
 kubectl create namespace argocd # add argocd namespace
 kubectl create namespace dev # add dev namespace
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml # install argocd in cluster
-kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}' # convert ui to loadbalancer
-kubectl -n argocd patch secret argocd-secret -p '{"stringData":  { "admin.password": "$2y$12$Kg4H0rLL/RVrWUVhj6ykeO3Ei/YqbGaqp.jAtzzUSJdYWT6LUh/n6", "admin.passwordMtime": "'$(date +%FT%T%Z)'" }}' # set argocd password to mysupersecretpassword
-kubectl patch configmap argocd-cm -n argocd -p '{"data":{"timeout.reconciliation": "60s"}}' # update timeout to 60s
+#kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml # install argocd in cluster
+#kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}' # convert ui to loadbalancer
+#kubectl -n argocd patch secret argocd-secret -p '{"stringData":  { "admin.password": "$2y$12$Kg4H0rLL/RVrWUVhj6ykeO3Ei/YqbGaqp.jAtzzUSJdYWT6LUh/n6", "admin.passwordMtime": "'$(date +%FT%T%Z)'" }}' # set argocd password to mysupersecretpassword
+#kubectl patch configmap argocd-cm -n argocd -p '{"data":{"timeout.reconciliation": "60s"}}' # update timeout to 60s
 
-./argo-pods-healthcheck.sh
+#./argo-pods-healthcheck.sh
 
-kubectl apply -f argocd-project.yml -n argocd # create project pointing to guribeir repo
-kubectl apply -f argocd-application.yml -n argocd # specify repo and application
+#kubectl apply -f argocd-project.yml -n argocd # create project pointing to guribeir repo
+#kubectl apply -f argocd-application.yml -n argocd # specify repo and application
+
+
+
+#kubectl apply -f dns-config-job.yaml na finalerraaa
+
+#INSTALAR O GITLABBB
+#helm repo add gitlab https://charts.gitlab.io/
+#helm repo update
+#helm install gitlab gitlab/gitlab -f gitlab-values.yml --namespace gitlab --create-namespace
+##afeter the isntalation:
+#kubectl apply -f gitlab-ingress.yaml (-n gitlab????)
+
+##esperar uma cota
+#kubectl get svc -n gitlab #pra achar o IP do gitlab-webservice ou algo do tipo
+# kubectl edit configmap coredns -n kube-system
+#substituir NodeHosts: |
+  #172.19.0.2 k3d-p3-cluster-server-0
+  #10.x.x.x gitlab.localhost
+#  10.43.138.207 gitlab.localhost
+#kubectl get pods -n gitlab
+#kubectl delete pod <NOMEEDO RUNNER POD->gitlab-gitlab-runner-55477f7b64-k54cz -n gitlab
+#kubectl delete pod gitlab-gitlab-runner-55477f7b64-fnfmv -n gitlab
+#RESOLLVEEUU
+# kubectl edit ingress gitlab-webservice-default -n gitlab
+
+#DELETAAR TUUDO
+#k3d cluster delete bonus-cluster
---
+++ bonus/to_delete/README.md
@@ -0,0 +1,56 @@
+# Helm chart to install Gitlab
+
+This chart is based on [my manifests](https://github.com/lwolf/kubernetes-gitlab) 
+for manual deployment with lots of improvements.
+
+ 
+Some of the features:
+* Fully automated installation of Gitlab. 
+* Register and manage Letsencrypt certificates with [kube-lego](https://github.com/jetstack/kube-lego)
+* Deploys PostgreSQL, Redis and Minio as part of the setup
+* Deploys fully configured Docker registry with external domain (letsencrypt) and integration with gitlab.
+* Semi-automatic creating of Runner
+* Ability to customize any of the [environment variables](https://github.com/sameersbn/docker-gitlab#available-configuration-parameters)
+
+## Requirements
+* Kubernetes 1.5 (for StatefulSets support).
+* PV support on the underlying infrastructure
+* Helm 2.2.0 (for conditions and flags support)
+
+## Quick installation
+
+```
+# add repository (needed for dependencies)
+$ helm repo add lwolf-charts http://charts.lwolf.org
+
+# get default values.yaml file
+$ curl https://raw.githubusercontent.com/lwolf/gitlab-chart/master/gitlab/values.yaml -o values-test.yaml
+
+# install
+$ helm install -f values-test.yaml lwolf-charts/gitlab
+
+# establish DNS
+# if running with minikube, add an entry in /etc/hosts to the result of `$ minikube ip`
+
+# Wait until gitlab is up and running.
+$ while ! curl --output /dev/null --silent --head --fail https://gitlab.example.com/help; do sleep 1 && echo -n .; done
+...................................................
+
+# Open gitlab runner configuration and check that Runner is successfully registered.
+# Copy token and run upgrade.
+$ helm upgrade -f values-test.yaml --set runner.token=<TOKEN> <install-name> lwolf-charts/gitlab --namespace=gitlab
+```
+
+## TODO:
+- [x] splitting registry/runner into separate charts
+- [x] Add support for gitlab pages
+- [x] Upgrade to v9
+- [x] Upgrade to Kubernetes 1.6+
+- [x] more flexible runner configuration
+- [ ] testing/refactoring
+- [ ] monitoring of the whole thing in Prometheus
+
+## Support on Beerpay
+Hey dude! Help me out for a couple of :beers:!
+
+[![Beerpay](https://beerpay.io/lwolf/gitlab-chart/badge.svg?style=beer-square)](https://beerpay.io/lwolf/gitlab-chart)  [![Beerpay](https://beerpay.io/lwolf/gitlab-chart/make-wish.svg?style=flat-square)](https://beerpay.io/lwolf/gitlab-chart?focus=wish)
